9fba1d2e35aa2e98d951f721b13a6ac0e1b96e31
ryannapp12 <ryannapp@gmail.com>
2026-03-05 17:58:44 +0000

    fix: use dependencyResolutionManagement for repos, remove allprojects

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,4 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
 plugins {
     id("com.android.library") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
 }
